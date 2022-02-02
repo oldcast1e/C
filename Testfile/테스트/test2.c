1ddb710d7dcf68b2baa144ad97b5156fd875db68
oldcast1e <kylhs0207@naver.com>
2022-02-03 00:22:40 +0900

    ud

--- Testfile/테스트/test2.c
+++ Testfile/테스트/test2.c
@@ -1,8 +1,27 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+struct A{
+    int data;
+    char name;
+}AA;
+
+// struct B{
+//     int data;
+//     char name[10];
+// }BB;
 int main(void){
-    
+   struct A *stra = &AA;
+   struct A *strb ;
+   stra->name = 'S';
+   stra->data = 1;
+
+   strb = stra;
+
+   strb->data = 3;
+   printf(">%d",stra->data);
+
+
     
    
 }
