0fb052911439424dd6a73e08acbf8a5770f65262
oldcast1e <kylhs0207@naver.com>
2022-03-23 14:24:30 +0900

    +

--- Testfile/테스트/test1.c
+++ Testfile/테스트/test1.c
@@ -1,23 +1,7 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 
-int main(){
-    int x;
-    scanf("%c", &x);
-
-    if('A'<= x && x <='Z')
-        printf("대문자입니다.");
-    else if('a' <= x && x <= 'z')
-        printf("소문자입니다.");
-    else if(0 <= x && x <= 9)
-        printf("숫자입니다.");
-    else
-    printf("사용하지 않는 키 입력");
-}
-
-/**
-10
-AF 10
-AF 20
-AF 30
-AR 40
- */
+int main() {
+    
+}	
