9cc68ccb5120d3bac7a5bb7601a66f7b769e1e28
oldcast1e <kylhs0207@naver.com>
2022-04-06 00:59:56 +0900

    +

--- Testfile/테스트/test1.c
+++ Testfile/테스트/test1.c
@@ -2,6 +2,22 @@
 #include<string.h>
 #include<stdlib.h>
 
-int main() {
+int main(){
+    int x, y, z,tmp, *px = &x, *py = &y, *pz = &z,*ptmp = &tmp;
 
-}	
+    scanf("%d",px);
+    scanf("%d",py);
+    scanf("%d",pz);
+
+    // *tmp = *py;
+    // *py = *px;
+    // *px = *pz;
+    // *pz = *tmp;
+
+    *ptmp = *px;
+    *px = *py;
+    *py = *pz;
+    *pz = *ptmp;
+
+    printf("%d %d %d\n", *px, *py, *pz);
+}
