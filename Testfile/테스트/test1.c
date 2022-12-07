35c7e2113faf2ff3fc3d5c6fa550afef88567562
oldcast1e <kylhs0207@naver.com>
2022-12-07 12:20:44 +0900

    new

--- Testfile/테스트/test1.c
+++ Testfile/테스트/test1.c
@@ -3,21 +3,5 @@
 #include<stdlib.h>
 
 int main(){
-    int x, y, z,tmp, *px = &x, *py = &y, *pz = &z,*ptmp = &tmp;
-
-    scanf("%d",px);
-    scanf("%d",py);
-    scanf("%d",pz);
-
-    // *tmp = *py;
-    // *py = *px;
-    // *px = *pz;
-    // *pz = *tmp;
-
-    *ptmp = *px;
-    *px = *py;
-    *py = *pz;
-    *pz = *ptmp;
-
-    printf("%d %d %d\n", *px, *py, *pz);
+    
 }
