087e15d1cbb8bb96923e1d0b3e273a086bdf6239
Lee Heonseong <kylhs0207@naver.com>
2023-10-03 13:17:27 +0000

    update

---
+++ Cprograming/Code Up/1733.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main(){
+    char arr[100];
+    int cnt = 0;
+    while(1){
+        scanf();
+        cnt ++;
+    }
+}   
+/**
+정보 선생님은 프로젝트 그룹 를 매우 싫어 한다.
+
+그 이유는 국제정보올림피아드(   :)의 이름과 동일해서,
+
+인터넷 검색창에 를 입력할 경우 더 이상 국제정보올림피아드에 대한 정보를 찾기가 힘들어 졌기 때문이다.
+
+입력으로 가 입력될 경우, IOI is the International Olympiad in Informatics.를 출력하는 프로그램을 작성하시오.
+
+만약 가 아닌 다른 글자가 들어오는 경우, I don't care.를 출력한다.*/
